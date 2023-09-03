8206a28a4a9ef2771f985042880fcf00c68fe4db
Ahmetyalcinkya <ahmetcan.yalcinkaya55@gmail.com>
2023-09-03 14:16:38 +0300

    Pagination

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import Header from "./components/Header";
 import Karakterler from "./components/Karakterler";
+import Pagination from "./components/Paging";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
   const [characters, setCharacters] = useState([]);
   const [search, setSearch] = useState("");
+  const [numberOfPages, setNumberOfPages] = useState(1);
+  const [activePage, setActivePage] = useState(1);
 
   const changeHandler = (e) => {
     const { value } = e.target;
@@ -16,9 +19,10 @@ const App = () => {
   };
   useEffect(() => {
     axios
-      .get("https://swapi.dev/api/people/")
+      .get("https://swapi.dev/api/people/" + activePage)
       .then((res) => {
         console.log(res.data);
+        setNumberOfPages(Math.ceil(res.data.count / res.data.results.length));
         const searchResults = res.data.filter((item) => {
           return (
             item.name.toLowerCase().includes(search.toLowerCase()) ||
@@ -30,8 +34,17 @@ const App = () => {
       .catch((err) => {
         console.error(err);
       });
-  }, [search]);
+  }, [search, activePage]);
 
+  const pageHandler = (page) => {
+    console.log(page);
+    if (page === "previous") {
+      page = activePage - 1 < 1 ? 1 : activePage - 1;
+    } else if (page === "next") {
+      page = activePage + 1 > numberOfPages ? numberOfPages : activePage + 1;
+    }
+    setActivePage(page);
+  };
   // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
@@ -40,6 +53,7 @@ const App = () => {
     <div className="App">
       <Header search={search} changeHandler={changeHandler} />
       <Karakterler characters={characters} />
+      <Pagination pageHandler={pageHandler} numberOfPages={numberOfPages} />
     </div>
   );
 };
